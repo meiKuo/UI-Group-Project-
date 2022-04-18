099f3a757265ebf21d2da86822ee722d747959d4
aditimutagi <68683431+aditimutagi@users.noreply.github.com>
2022-04-18 15:18:44 -0500

    Update homepage.js

--- static/homepage.js
+++ static/homepage.js
@@ -1,1 +1,70 @@
 //fill dynamically when data is filled
+
+
+
+			$(document).ready(function(){
+    			$.each(lessons, function(index, l) {
+        			$('#Overview').append(
+        					$('<div>').prop({
+                					class: 'row'
+        					}).append(
+            					$('<div>').prop({
+                					class: 'col-md-4'
+            					})
+        					
+        					).append(
+            					$('<div>').prop({
+                					class: 'col-md-4 title padButtons'
+            					}).append(
+                					$('<a>').prop({
+                    					href: "/lesson" + (index + 1).toString(),
+                    					class: "right",
+                    					id: "a"
+                					})
+            					).append(
+                					$('<button>').prop({
+                    					class: "button",
+                    					id: "lesson"+(index + 1).toString(),
+                    					innerHTML: "Lesson " + (index + 1).toString() + ": " + l
+                					})
+            					
+            					)
+        					
+        					)
+    					)
+    					
+    					
+    			
+    			
+    			})
+    			
+    			$('#Overview').append(
+    					$('<div>').prop({
+            					class: 'row'
+    					}).append(
+        					$('<div>').prop({
+            					class: 'col-md-4'
+        					})
+    					
+    					).append(
+        					$('<div>').prop({
+            					class: 'col-md-4 title padButtons'
+        					}).append(
+            					$('<button>').prop({
+                					class: "button",
+                					innerHTML: "Adventure Time!"
+            					})
+        					
+        					)
+    					
+    					)
+					)
+				
+				
+
+    
+
+			})
+			
+			
+			
