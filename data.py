18eb641fed60da8b39a8aaac84d004ba5e78e295
aditimutagi <68683431+aditimutagi@users.noreply.github.com>
2022-04-25 00:12:01 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,130 +1,89 @@
 mushrooms = {
     "0": {
         "id": "0",
-        "name": "Chanterelle",
+        "name": "Black Morel",
         "edible": True,
-        "img_url": 'https://thumbs.dreamstime.com/z/chanterelle-mushroom-hand-drawn-vector-illustration-sketch-food-drawing-isolated-white-background-organic-vegetarian-product-147115100.jpg',
+        "img_url": 'https://cdn.dribbble.com/users/2860422/screenshots/9385109/media/dc407522c3ef84385e1c7cdcb01d90e6.png?compress=1&resize=400x300',
         "cut_img_url": "images/morel_1.png",
-        "cap": "Funnel shaped, wavy cap", 
-        "stem": "full stem", 
-        "gills": "Blunt, gill-like ridges", 
-        "smellTaste": "Peppery/peachy/apricot flavor", 
-        "color": "Bright yellow/orange color", 
-        "location": "Found on the East and west coasts", 
-        "internalColor": "White internal flesh", 
+        "features": [
+            "Irregularly shaped",
+            "Bulging outwards",
+            "Wavy and lobed",
+            "Hangs freely from the stem"
+        ]
     },
     "1": {
-        "id": "0",
-        "name": "Jack o Lantern",
-        "edible": False,
-        "img_url": 'https://baynature.org/wp-content/uploads/2018/10/jackolantern.jpg',
+        "id": "1",
+        "name": "False Morel",
+        "edible": True,
+        "img_url": 'https://as1.ftcdn.net/v2/jpg/02/94/34/30/1000_F_294343087_4w1GR3vVswC4RoOslk9VxORUxiRb20Rw.jpg',
         "cut_img_url": "images/morel_1.png",
-        "cap": "Round, flat", 
-        "stem": "Hollow stem", 
-        "gills": "True, sharp gills on stalk", 
-        "smellTaste": "N/Ar", 
-        "color": "Orangy brown", 
-        "location": "East & West of Rocky Mts", 
-        "internalColor": "Orange internal flesh", 
+        "features": [
+            "Irregularly shaped",
+            "Bulging outwards",
+            "Wavy and lobed",
+            "Hangs freely from the stem"
+        ]
     }, 
     "2": {
         "id": "2",
-        "name": "Destoying Angel",
-        "edible": False,
-        "img_url": 'https://media.illustrationx.com/images/artist/LizPepperell/128015/watermark/1300/destroying-angel-toadstool.jpg',
+        "name": "Death Cap",
+        "edible": True,
+        "img_url": 'https://cdn-s-www.bienpublic.com/images/8DF87B22-AADD-4FC0-8FC7-8632C3480795/NW_raw/les-champignons-tels-que-les-quot-psilocybe-semilanceata-quot-sont-inscrits-sur-la-liste-des-stupefiants-depuis-1990-en-france-photo-d-illustration-pixabay-domaine-public-1507911226.jpg',
         "cut_img_url": "images/morel_1.png",
-        "cap": "Pure white stalks cap", 
-        "stem": "Pure white stems", 
-        "gills": "Pure white gills", 
-        "smellTaste": "N/A", 
-        "color": "White", 
-        "location": "Near woodlands or shrubs in suburban lawns/meadows", 
-        "internalColor": "White", 
+        "features": [
+            "Irregularly shaped",
+            "Bulging outwards",
+            "Wavy and lobed",
+            "Hangs freely from the stem"
+        ]
     },
     "3": {
         "id": "3",
         "name": "Giant Puffball",
         "edible": True,
+        "poisonous": False,
         "img_url": 'images/puffball_mushroom_1.png',
         "cut_img_url": "images/puffball_mushroom_open_1.png",
-        "cap": "Pure white stalks cap", 
-        "stem": "Pure white stems", 
-        "gills": "N/A", 
-        "smellTaste": "Taste rich, earthy, and nutty", 
-        "color": "White", 
-        "location": "Found in meadows, fields, and deciduous forests", 
-        "internalColor": "White", 
+        "features": [
+            "Irregularly shaped",
+            "Bulging outwards",
+            "Wavy and lobed",
+            "Hangs freely from the stem"
+        ]
     },
     "4": {
         "id": "4",
-        "name": "Lion's Mane",
-        "edible": True,
-        "img_url": 'https://image.shutterstock.com/z/stock-vector-lion-s-mane-mushroom-hericium-erinaceus-edible-and-medicinal-plant-hand-drawn-botanical-vector-1328148566.jpg',
-        "cut_img_url": "images/puffball_mushroom_open_1.png",
-        "cap": "Distinctive shape that resembles mane of a male lion or a pom pom", 
-        "stem": "Pure white stems", 
-        "gills": "Instead of gills, this mushroom body has little teeth that the spores are released from.", 
-        "smellTaste": "Tastes like seafood", 
-        "color": "White", 
-        "location": "Often found on beech trees", 
-        "internalColor": "White", 
-    },
+        "name": "Chanterelle",
+        "img_url": "https://media.istockphoto.com/photos/two-yellow-chantarelles-picture-id133823186?k=20&m=133823186&s=612x612&w=0&h=FQj2DXE24LGlV99r51CgsCeRufp1EdUNZdxJMQkBq0I=",
+        "cut_img_url": "",
+        "edible": "Edible",
+        "cap": "Funnel shaped, wavy",
+        "stem": "Full stem",
+        "gills": "Blunt, gill-like ridges",
+        "taste": "Peppery/peachy/apricot",
+        "color": "Bright yellow/orange",
+        "location": "East and West Coasts",
+        "inner": "White internal flesh"
+        
+        },
     "5": {
         "id": "5",
-        "name": "Maitake Mushrooms",
-        "edible": True,
-        "img_url": 'https://thumbs.dreamstime.com/z/maitake-mushroom-grifola-frondosa-hen-woods-medicinal-plant-maitake-mushroom-grifola-frondosa-hen-woods-109414751.jpg',
-        "cut_img_url": "images/puffball_mushroom_open_1.png",
-        "cap": "Distinctive shape that resembles mane of a male lion or a pom pom", 
-        "stem": "Woody stem", 
-        "gills": "No gills, but leave a white spore print", 
-        "smellTaste": "Strong earthy, peppery flavor", 
-        "color": "Brown", 
-        "location": "Mainly found in the northeast", 
-        "internalColor": "White", 
-    },
-    "6": {
-        "id": "6",
-        "name": "Oyster Mushrooms",
-        "edible": True,
-        "img_url": 'https://thumbs.dreamstime.com/z/oyster-mushroom-hand-drawn-vector-illustration-sketch-food-drawing-isolated-white-background-organic-vegetarian-product-great-146858794.jpg',
-        "cut_img_url": "images/puffball_mushroom_open_1.png",
-        "cap": "Scalloped, whitish-grey (sometimes tan) caps", 
-        "stem": "Woody stem", 
-        "gills": "White, decurrent gills", 
-        "smellTaste": "Delicate and savory, with a hint of complexity on the finish", 
-        "color": "Grayish-brown", 
-        "location": "Found on dying hardwood trees like oaks, maples, etc especially after it rains", 
-        "internalColor": "White", 
-    },
-    "7": {
-        "id": "7",
-        "name": "True Morels",
-        "edible": True,
-        "img_url": 'https://cdn3.vectorstock.com/i/1000x1000/99/27/hand-drawn-colorful-morel-mushroom-vector-27769927.jpg',
-        "cut_img_url": "images/puffball_mushroom_open_1.png",
-        "cap": "Scalloped, whitish-grey (sometimes tan) caps", 
-        "stem": "Pebbly texture", 
-        "gills": "Conical or oval Attached cap", 
-        "smellTaste": "Earthy, woodsy, and nutty", 
-        "color": "Range in color: from cream to black", 
-        "location": "Found in moist areas and on specific trees like maple, conifers, and oak", 
-        "internalColor": "White", 
-    },
-    "8": {
-        "id": "8",
-        "name": "False Morels",
-        "edible": False,
-        "img_url": 'https://thumbs.dreamstime.com/z/gyromitra-gigas-snow-false-morel-calf-brain-bull-nose-mushroom-closeup-digital-art-illustration-boletus-has-orange-winding-cap-210967375.jpg',
-        "cut_img_url": "images/puffball_mushroom_open_1.png",
-        "cap": "Brain-like or saddle-shaped structure", 
-        "stem": "Cotton-like white stem when sliced down the middle", 
-        "gills": "N/A", 
-        "smellTaste": "N/A", 
-        "color": "Reddish or a brownish redn", 
-        "location": "Grow in disturbed soil across North America and Europe.", 
-        "internalColor": "White", 
-    },
+        "name": "Jack o Lantern",
+        "img_url": "https://healing-mushrooms.net/wp-content/uploads/2020/01/Jack-o-Lantern-Mushroom.jpg",
+        "cut_img_url": "",
+        "edible": "Toxic",
+        "cap": "Round, flat",
+        "stem": "Hollow stem",
+        "gills": "True, sharp gills on stalk",
+        "taste": "N/A",
+        "color": "Orangy brown",
+        "location": "East & West of Rocky Mts",
+        "inner": "Orange internal flesh"
+        
+        
+        
+        }
 
 }
 
@@ -230,37 +189,37 @@ questions = [
 lessons = [
     {
         "lesson_id": "1", 
-        "lesson_name": "True vs False Morels", 
-        "mushroom1": "7",
-        "mushroom2": "8" , 
+        "lesson_name": "True vs False Morels ", 
+        "mushroom1": "0",
+        "mushroom2": "1",
+        "type": "compare"
     }, 
     {
         "lesson_id": "2", 
         "lesson_name": "Chanterelle vs Jack o Lantern", 
-        "mushroom1": "0",
-        "mushroom2": "1" , 
+        "mushroom1": "4",
+        "mushroom2": "5",
+        "type": "compare"
     }, 
+    #Fix lessons 3 onwards based on the format for lessons 1 and 2
     {
         "lesson_id": "3", 
-        "lesson_name": "Puff Ball vs Destroying Angel", 
-        "mushroom1": "2",
-        "mushroom2": "3" , 
+        "lesson_name": " 3. Puff Ball vs Death Cap vs Destroying Angel", 
+        "type": "compare"
     }, 
     {
         "lesson_id": "4", 
-        "lesson_name": "Lion's Mane", 
-        "mushroom1": "4",
+        "lesson_name": "Maitake Mushrooms", 
+        "type": "indiv"
     }, 
     {
         "lesson_id": "5", 
-        "lesson_name": "Maitake Mushrooms", 
-        "mushroom1": "5",
-    }, 
-    {
-        "lesson_id": "6", 
         "lesson_name": "Oyster Mushrooms", 
-        "mushroom1": "6",
-
-    }, 
+        "type": "indiv"
+    }, {
+        "lesson_id": "6", 
+        "lesson_name": "Lion's Mane", 
+        "type": "indiv"
+    },
     
 ]
