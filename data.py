d04a5996908a6918025f3dbe928b2be2e66e5114
meiKuo <72627349+meiKuo@users.noreply.github.com>
2022-04-26 12:37:22 -0400

    Complete the first round of lessons overview

--- data.py
+++ data.py
@@ -176,35 +176,59 @@ lessons = [
     },
     {
         "lesson_id": "2",
-        "lesson_name": "Chanterelle vs Jack o Lantern",
+        "lesson_name": "Chanterelle",
         "mushroom1": "4",
-        "mushroom2": "5",
-        "type": "compare"
+        "img_url": "https://media.istockphoto.com/photos/two-yellow-chantarelles-picture-id133823186?k=20&m=133823186&s=612x612&w=0&h=FQj2DXE24LGlV99r51CgsCeRufp1EdUNZdxJMQkBq0I=",
+        "type": "indiv"
     },
     {
         "lesson_id": "3",
-        "lesson_name": "Puff Ball vs Destroying Angel",
-        "mushroom1": "2",
-        "mushroom2": "3" ,
-        "type": "compare",
+        "lesson_name": "Puff Ball",
+        "mushroom1": "3",
+        "img_url": 'https://education.mdc.mo.gov/sites/default/files/styles/centered_full/public/media/field-guide/Puffball.jpg?itok=6avLAqkN',
+        "type": "indiv",
     },
     {
         "lesson_id": "4",
         "lesson_name": "Maitake Mushrooms",
         "mushroom1": "6",
+        "img_url": 'https://thumbs.dreamstime.com/z/maitake-mushroom-grifola-frondosa-hen-woods-medicinal-plant-maitake-mushroom-grifola-frondosa-hen-woods-109414751.jpg',
         "type": "indiv",
     },
     {
         "lesson_id": "5",
         "lesson_name": "Oyster Mushrooms",
         "mushroom1": "7",
+        "img_url": 'https://thumbs.dreamstime.com/z/oyster-mushroom-hand-drawn-vector-illustration-sketch-food-drawing-isolated-white-background-organic-vegetarian-product-great-146858794.jpg',
         "type": "indiv"
     },
     {
         "lesson_id": "6",
         "lesson_name": "Lion's Mane",
         "mushroom1": "8",
+        "img_url": 'https://image.shutterstock.com/z/stock-vector-lion-s-mane-mushroom-hericium-erinaceus-edible-and-medicinal-plant-hand-drawn-botanical-vector-1328148566.jpg',
         "type": "indiv"
+    },
+    {
+        "lesson_id": "7",
+        "lesson_name": "False Morels ",
+        "mushroom1": "1",
+        "img_url": 'https://as1.ftcdn.net/v2/jpg/02/94/34/30/1000_F_294343087_4w1GR3vVswC4RoOslk9VxORUxiRb20Rw.jpg',
+        "type": "indiv", 
+    },
+    {
+        "lesson_id": "8",
+        "lesson_name": "Jack o Lantern",
+        "mushroom1": "5",
+        "img_url": "https://healing-mushrooms.net/wp-content/uploads/2020/01/Jack-o-Lantern-Mushroom.jpg",
+        "type": "indiv", 
+    },
+    {
+        "lesson_id": "9",
+        "lesson_name": "Destroying Angel",
+        "mushroom1": "2",
+       "img_url": 'https://media.illustrationx.com/images/artist/LizPepperell/128015/watermark/1300/destroying-angel-toadstool.jpg',
+        "type": "indiv", 
     },
     # {
     #     "lesson_id": "1",
