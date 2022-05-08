26ffb9f7d7897e5c51456dfd5161d39d9a47b549
aditimutagi <68683431+aditimutagi@users.noreply.github.com>
2022-05-08 02:42:23 -0400

    Update data.py

--- data.py
+++ data.py
@@ -3,7 +3,7 @@ mushrooms = {
         "id": "0",
         "name": "Black Morel",
         "edible": True,
-        "img_url": '/static/images/blackMorel.png',
+        "img_url": '/static/images/black_morel.jpeg',
         "cut_img_url": "/static/images/blackmorel_cut.png",
         "cap": "Scalloped, whitish-grey (sometimes tan) caps",
         "stem": "Pebbly texture",
@@ -12,13 +12,13 @@ mushrooms = {
         "color": "Range in color: from cream to black",
         "location": "Found in moist areas and on specific trees like maple, conifers, and oak",
         "inner": "White",
-        "description": "Black Morels are unlike most of our common mushrooms. They lack gills, pores, or teeth to drop spores.",
+        "description": "Morchella elata, Black Morels, tend to appear during March to June and are a popular edible fungus. They are typically found in woods and forests, and tend to grow in groups. Unlike most other common mushrooms, they lack gills, pores, or teeth to drop spores which makes it hard to guarantee their appearance every year."
     },
     "1": {
         "id": "1",
         "name": "False Morel",
         "edible": False,
-        "img_url": '/static/images/False Morels.png',
+        "img_url": '/static/images/falsemorel.jpeg',
         "cut_img_url": "/static/images/falseMorel_cut.png",
         "cap": "Brain-like or saddle-shaped structure",
         "stem": "Cotton-like white stem when sliced down the middle",
@@ -27,13 +27,13 @@ mushrooms = {
         "color": "Reddish or a brownish redn",
         "location": "Grow in disturbed soil across North America and Europe.",
         "inner": "White",
-        "description": "The False Morel is a Spring mushroom that contains the toxin gyromitrin. Raw or cooked - it can be very toxic, even deadly when enough is consumed.",
+        "description": "The False Morel is a Spring mushroom that contains the toxin gyromitrin, a carcinogenci chemical. If handled improperly, it can have severe side-effects, including death. Though there are ways to consume this mushroom safely, such as through repeated boiling, consumption of even small traces of this chemical can lead to serious consequences. Thus, to beginner shroomers, it would be best to avoid consuming and smelling the fumes of this mushroom type."
     },
     "2": {
         "id": "2",
         "name": "Destroying Angel",
         "edible": False,
-        "img_url": '/static/images/destroyingangel.png',
+        "img_url": '/static/images/destroying_angel.jpeg',
         "cut_img_url": "/static/images/destroyingAngel_cut.png",
         "cap": "Pure white stalks cap",
         "stem": "Pure white stems",
@@ -42,7 +42,8 @@ mushrooms = {
         "color": "White",
         "location": "Near woodlands or shrubs in suburban lawns/meadows",
         "inner": "White",
-        "description": "Destroying angels are among the most toxic known mushrooms. The destroying angel is all white, with a ring on the stalk and a large, saclike cup around the base of the stalk.",
+        "description": "Destroying angels are among the most toxic known mushrooms. The destroying angel is all white, with a ring on the stalk and a large, saclike cup around the base of the stalk. Despite having caused many deaths, there doesn't exist a known antidote to this mushroom. Symptoms include: severe vomiting, diarrhea, and abdominal pains. While this is often followed by what seems to be a steady recovery for some days, this sadly ends in kidney and liver failure, resulting in death."
+
     },
     "3": {
         "id": "3",
@@ -57,7 +58,7 @@ mushrooms = {
         "color": "White",
         "location": "Found in meadows, fields, and deciduous forests",
         "inner": "White",
-        "description":"Giant puffball mushrooms can be found all over the country, but mostly in the midwest from the summer until about October.",
+        "description":"Giant puffball mushrooms can be found all over the country, but mostly in the midwest from the summer until about October. These fungi are commonly used by Native American tribes, especially the Cherokee, who use them as a styptic to stop wounds from bleeding and to heal sores/burns. In addition to its medicinal benefits, puffballs are notably antibacterial and (ironically) antifungal.",
     },
     "4": {
         "id": "4",
@@ -72,15 +73,13 @@ mushrooms = {
         "color": "Bright yellow/orange",
         "location": "East and West Coasts",
         "inner": "White internal flesh",
-        "description": "Chanterelles are known for their beautiful yellow-orange color, their rarity, and most of all, their flavor.",
-
-
+        "description": "Chanterelles are known for their beautiful yellow-orange color, their rarity, and most of all, their flavor. Unfortunately, due to their symbiotic relation with tree roots, they can be difficult (nearly impossible) to cultivate. Thus, even store-bought chanterelles tend to be picked from the wild. Their most notable property, however, is their insecticidal characteristic, allowing them to resist rot and bugs."
         },
     "5": {
         "id": "5",
         "name": "Jack o Lantern",
         "img_url": "/static/images/jackolantern.png",
-        "cut_img_url": "/static/images/jackolantern_cut.png",
+        "cut_img_url": "/static/images/jackolantern_cut.jpeg",
         "edible": False,
         "cap": "Round, flat",
         "stem": "Hollow stem",
@@ -89,14 +88,14 @@ mushrooms = {
         "color": "Orangy brown",
         "location": "East & West of Rocky Mts",
         "inner": "Orange internal flesh",
-        "description": "Jack o' Lantern is a bright orange mushroom that usually grows in dense tufts from the decaying underground roots of trees or from dead stumps.",
+        "description": "Jack o' Lantern is a bright orange mushroom that usually grows in dense tufts from the decaying underground roots of trees or from dead stumps. One of its most notable properties is the bioluminescence of its gills, a blue-green that is observed in the dark. This property is caused by luciferase, an enzyme that also causes the glowing ability in fireflies."
         },
     "6": {
         "id": "6",
         "name": "Maitake Mushroom",
         "edible": True,
-        "img_url": '/static/images/maitake.png',
-        "cut_img_url": "/static/images/maitake_cut.png",
+        "img_url": '/static/images/maitake.jpeg',
+        "cut_img_url": "/static/images/maitake_cut.jpeg",
         "cap": "Distinctive shape that resembles mane of a male lion or a pom pom",
         "stem": "Woody stem",
         "gills": "No gills, but leave a white spore print",
@@ -104,14 +103,14 @@ mushrooms = {
         "color": "Brown",
         "location": "Mainly found in the northeast",
         "inner": "White",
-        "description": "Maitake” means dancing mushroom in Japanese. The mushroom is said to have gotten its name after people danced with happiness upon finding it in the wild.",
+        "description":"Matiake (\" dancing \" in Japenese) mushrooms got their name from the legend that people danced with joy once they discovered it in the wild. Its prominently known as an adaptogen, helping the body fight mental or physical ailments. Specifically, they are known to help regulate unbalanced systems within the body. Due to such properties, it is often regarded as a medicinal mushroom that can enhance the nutrition of any dish that uses it."
 
     },
     "7": {
         "id": "7",
         "name": "Oyster Mushroom",
         "edible": True,
-        "img_url": '/static/images/oystermushroom.png',
+        "img_url": '/static/images/oyster.jpeg',
         "cut_img_url": "/static/images/oyster_cut.png",
         "cap": "Scalloped, whitish-grey (sometimes tan) caps",
         "stem": "Woody stem",
@@ -120,13 +119,13 @@ mushrooms = {
         "color": "Grayish-brown",
         "location": "Found on dying hardwood trees like oaks, maples, etc especially after it rains",
         "inner": "White",
-        "description": "Oyster mushrooms have a white to light brown to a darker brown, funnel-shaped cap, with whitish- yellow gills running up a short off-center stem. The flesh is white.",
+        "description": "Oyster mushrooms have a white to light brown to a darker brown, funnel-shaped cap, with whitish- yellow gills running up a short off-center stem. As one of the most cultivated mushrooms around the world, they are common to many cuisines, specifically Chinese, Japenses, and Korean cooking. Oyster mushrooms are uniquely one of the few carnivorous mushrooms, feeding on bacteria and roundworms. Due to their ability to speed up decomposition rates, oyster mushrooms could possibly be a way to remove unwanted toxins from the environment."
     },
     "8": {
         "id": "8",
         "name": "Lion's Mane",
         "edible": True,
-        "img_url": '/static/images/lionsmane.png',
+        "img_url": '/static/images/lionsmane.jpeg',
         "cut_img_url": "/static/images/lions_mane_lessonCut.png",
         "cap": "Distinctive shape that resembles mane of a male lion or a pom pom",
         "stem": "Pure white stems",
@@ -135,7 +134,7 @@ mushrooms = {
         "color": "White",
         "location": "Often found on beech trees",
         "inner": "White",
-        "description": "The lion's mane mushroom is a species known by a variety of names. Lion’s mane mushrooms are native to the mountains of northeast Asia and may also be found in Europe and North America.",
+        "description": "The lion's mane mushroom is a species known by a variety of names. Lion’s mane mushrooms are native to the mountains of northeast Asia and may also be found in Europe and North America. Its flavor is often described as “seafood-like,” and can be consumed raw, cooked, or even in tea. Lion's mane mushrooms are well known for their health benefits, such as preventing dementia, relieving mild depression and anxiety, and reducing the risk of heart disease."
     },
 
 }
@@ -173,7 +172,7 @@ quiz = {
         "id": "5",
         "mushroom_id": "4",
         "img_url": 'images/chanterelle_1.jpeg',
-        "cut_img_url": "images/chanterelle_quiz_open.png",
+        "cut_img_url": "images/chanterell_open_1.png",
         "done": False
     },
     "6": {
@@ -208,7 +207,7 @@ quiz = {
         "id": "10",
         "mushroom_id": "7",
         "img_url": 'images/oyster_1.jpeg',
-        "cut_img_url": "images/oyster_cut_lesson.png",
+        "cut_img_url": "images/oyster_1.jpeg",
         "done": False
     },
     "11": {
@@ -234,7 +233,7 @@ lessons = {
         "lesson_id": "1",
         "lesson_name": "True Morel",
         "mushroom": "0",
-        "img_url": 'https://media.istockphoto.com/vectors/hand-drawn-colorful-morel-mushroom-vector-id1181276056?k=6&m=1181276056&s=612x612&w=0&h=_qqA5l8WiwQpR2mRJc0mnndmzgyky-EZs_nTwl21T0U=',
+        "img_url": '/static/images/black_morel.jpeg',
         "type": "indiv",
     },
     "2": {
@@ -255,42 +254,42 @@ lessons = {
         "lesson_id": "4",
         "lesson_name": "Maitake Mushroom",
         "mushroom": "6",
-        "img_url": '/static/images/maitake.png',
+        "img_url": '/static/images/maitake.jpeg',
         "type": "indiv",
     },
     "5": {
         "lesson_id": "5",
         "lesson_name": "Oyster Mushroom",
         "mushroom": "7",
-        "img_url": '/static/images/oystermushroom.png',
+        "img_url": '/static/images/oyster.jpeg',
         "type": "indiv"
     },
     "6": {
         "lesson_id": "6",
         "lesson_name": "Lion's Mane",
         "mushroom": "8",
-        "img_url": '/static/images/lionsmane.png',
+        "img_url": '/static/images/lionsmane.jpeg',
         "type": "indiv"
     },
     "7": {
         "lesson_id": "7",
         "lesson_name": "False Morel",
         "mushroom": "1",
-        "img_url": 'https://as1.ftcdn.net/v2/jpg/02/94/34/30/1000_F_294343087_4w1GR3vVswC4RoOslk9VxORUxiRb20Rw.jpg',
+        "img_url": '/static/images/falsemorel.jpeg',
         "type": "indiv",
     },
     "8": {
         "lesson_id": "8",
         "lesson_name": "Jack o Lantern",
         "mushroom": "5",
-        "img_url": "https://healing-mushrooms.net/wp-content/uploads/2020/01/Jack-o-Lantern-Mushroom.jpg",
+        "img_url": "/static/images/jackolantern.png",
         "type": "indiv",
     },
     "9": {
         "lesson_id": "9",
         "lesson_name": "Destroying Angel",
         "mushroom": "2",
-        "img_url": "https://media.illustrationx.com/images/artist/LizPepperell/128015/watermark/1300/destroying-angel-toadstool.jpg",
+        "img_url": "static/images/destroying_angel.jpeg",
         "type": "indiv",
     },
 }
