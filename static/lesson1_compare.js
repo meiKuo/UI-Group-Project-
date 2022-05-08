b901ad12c39342b6cc0438c13cdace0b85aa5106
aditimutagi <68683431+aditimutagi@users.noreply.github.com>
2022-05-08 05:46:38 -0400

    Update lesson1_compare.js

--- static/lesson1_compare.js
+++ static/lesson1_compare.js
@@ -1,52 +1,73 @@
 //fill dynamically when data is filled
+
+
+
 $(document).ready(() => {
-    let lesson_name = lesson_params["lesson_name"]
-    let description = lesson_params["description"]
     let mushroom1 = lesson_params["mushroom1"]
     let mushroom2 = lesson_params["mushroom2"]
+    let lessons = lesson_params["lessons"]
+    
+    for (let value in lessons) {
+        if (lessons[value]["mushroom"] == mushroom1["id"]){
+            let back = '"/lesson/' + value + '"'
+            document.getElementById("back").setAttribute('onclick','window.location.href = ' + back)
+        }
+            
+    }
+    
+    
 
-    $("#backButton").click(function(){
-      window.location.href ='/lessonplans'
-    })
-
-    document.getElementById("name1").innerHTML = mushroom1["name"]
-    document.getElementById("name2").innerHTML = mushroom2["name"]
-
-    document.getElementById("img1").src = mushroom1["img_url"]
-    document.getElementById("img2").src = mushroom2["img_url"]
-
+    document.getElementById("title").innerHTML = mushroom1["name"] + " vs " + mushroom2["name"];
+    
+    
+    
     if (mushroom1["edible"]){
-      document.getElementById("edible1").innerHTML = "Not Toxic"
+        document.getElementById("banner1").style.backgroundColor = "#96D365";
+        document.getElementById("banner1").innerHTML = "Edible"
     }
     else{
-      document.getElementById("edible1").innerHTML = "Toxic"
+        document.getElementById("banner1").style.backgroundColor = "#F68780";
+        document.getElementById("banner1").innerHTML = "Poisonous"
     }
+    
     if (mushroom2["edible"]){
-      document.getElementById("edible2").innerHTML = "Not Toxic"
+        document.getElementById("banner2").style.backgroundColor = "#96D365";
+        document.getElementById("banner2").innerHTML = "Edible"
     }
     else{
-      document.getElementById("edible2").innerHTML = "Toxic"
+        document.getElementById("banner2").style.backgroundColor = "#F68780";
+        document.getElementById("banner2").innerHTML = "Poisonous"
     }
+    
+    
+    document.getElementById("img1").src = mushroom1["img_url"]
 
+    document.getElementById("img2").src = mushroom2["img_url"]
+    
+    
     document.getElementById("cap1").innerHTML = mushroom1["cap"]
+    
     document.getElementById("cap2").innerHTML = mushroom2["cap"]
-
+    
     document.getElementById("stem1").innerHTML = mushroom1["stem"]
+    
     document.getElementById("stem2").innerHTML = mushroom2["stem"]
-
+    
     document.getElementById("gills1").innerHTML = mushroom1["gills"]
+    
     document.getElementById("gills2").innerHTML = mushroom2["gills"]
-
-    document.getElementById("taste1").innerHTML = mushroom1["taste"]
-    document.getElementById("taste2").innerHTML = mushroom2["taste"]
-
+    
     document.getElementById("color1").innerHTML = mushroom1["color"]
+    
     document.getElementById("color2").innerHTML = mushroom2["color"]
-
-    document.getElementById("location1").innerHTML = mushroom1["location"]
-    document.getElementById("location2").innerHTML = mushroom2["location"]
-
+    
     document.getElementById("inner1").innerHTML = mushroom1["inner"]
+    
     document.getElementById("inner2").innerHTML = mushroom2["inner"]
-
+    
+    
+    
+    
+    
+    
   })
